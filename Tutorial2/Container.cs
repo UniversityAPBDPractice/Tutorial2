ac0439d8c0e9d39f3f9af4f1c5e1cd2f9aa7ca0e
Oleksandr <milkoaleksandr307@gmail.com>
2025-03-18 09:38:39 +0100

    Init Container, OverfillException

---
+++ Tutorial2/Container.cs
@@ -0,0 +1,44 @@
+﻿namespace Tutorial2;
+
+public abstract class Container
+{
+    public enum Type
+    {
+        L,
+        G,
+        C
+    }
+
+    private static int counter = 0;
+    public required float Mass { get; set; }
+    public required float Height { get; set; }
+    public required float TareWeight { get; set; }
+    public required float CargoWeight { get; set; }
+    public required float Depth { get; set; }
+    public required float MaxPayload { get; set; }
+    public required Type ContainerType;
+    public required int Id;
+
+    public static int IncrementCounter()
+    {
+        return counter++;
+    }
+
+    public String GetSerialNumber()
+    {
+        return $"KON-{ContainerType}-{Id}";
+    }
+
+    public void Empty()
+    {
+        CargoWeight = 0;
+    }
+
+    public void Load(float Mass)
+    {
+        if (Mass > MaxPayload)
+        {
+            throw new OverfillException("The Mass you specified exceeds Payload Capacity");
+        }
+    }
+}
---
+++ Tutorial2/OverfillException.cs
@@ -0,0 +1,7 @@
+﻿namespace Tutorial2;
+
+public class OverfillException : Exception
+{
+    public OverfillException() : base() {}
+    public OverfillException(String message) : base(message) {}
+}
